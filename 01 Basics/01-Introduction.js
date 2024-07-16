075d04c8886481ffc39deee4d69dbe07616da8cd
Hitesh Mahto <hiteshmahto786@gmail.com>
2024-07-16 12:31:25 +0530

    intro and setup

---
+++ 01 Basics/01-Introduction.js
@@ -0,0 +1,10 @@
+console.log("Hello Hitesh!!!");
+/*
+JavaScript code is executed directly by a JavaScript engine within a web browser or a runtime environment like Node.js. Major browsers (such as Chrome, Firefox, Safari, and Edge) have their own JavaScript engines:
+
+- Chrome uses the V8 engine.
+- Safari uses the JavaScriptCore engine, also known as Nitro.
+- Edge uses the Chakra engine, though newer versions use V8 as they are based on Chromium.
+
+Node.js also uses the V8 engine for server-side JavaScript execution. These engines parse and execute JavaScript code directly, optimizing it on-the-fly to improve performance.
+*/
